13a3d6c866230c392780bba43ad3384f66e6e184
xtrs84zk <xtrs84zk@gmail.com>
2021-01-02 13:18:24 -0600

    Audit modules.

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const { version } = require('./package.json')
 
 module.exports = {
-  baseUrl: '/retrospective/',
+  publicPath: '/retrospective/',
 
   configureWebpack: {
     plugins: [
